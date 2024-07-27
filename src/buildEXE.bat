bae952f03824cb5b67efdbd4a335474da0236985
GinoLin980 <ppple1872@gmail.com>
2024-07-27 18:36:34 +0800

    added website

--- src/buildEXE.bat
+++
@@ -1,5 +0,0 @@
-pyinstaller --onefile --icon="utils/car.ico" -w --add-data utils;utils --splash "utils/splash.png" --name="FHGearbox" FH_auto_classes.py
-rmdir /s /q "build"
-move "dist\FHGearbox.exe" .
-rmdir /s /q "dist"
-del "FHGearbox.spec"
