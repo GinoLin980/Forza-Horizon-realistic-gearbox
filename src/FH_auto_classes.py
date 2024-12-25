5ac559cb03aa322448eab3ff695bd83988371409
GinoLin980 <ppple1872@gmail.com>
2024-12-25 14:38:18 +0800

    Fix indentation in rpm_range_bottom calculation in Gearbox class

--- src/FH_auto_classes.py
+++ src/FH_auto_classes.py
@@ -137,7 +137,7 @@ class Gearbox():
         # adjust the allowed downshifting rpm range,
         # depending on the aggressiveness
         #(-400 for lower rpm cars and it looks if the rpm range is below or = 6200) else it defaults to normal self.rpm_range_bottom 
-         self.rpm_range_bottom = max(idleRPM + (min(self.gear, 6) * 70) * 0.96, self.rpm_range_top - self.rpm_range_size)
+        self.rpm_range_bottom = max(idleRPM + (min(self.gear, 6) * 70) * 0.96, self.rpm_range_top - self.rpm_range_size)
         if (self.rpm_range_top <= 6200):
             self.rpm_range_bottom -= 400
         # (slip condition)
